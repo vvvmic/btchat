f3b653010c71e2f948f2f27d0f56b8659e25fafc
vlaeden <46054577+vlaeden@users.noreply.github.com>
2022-11-29 20:09:43 +0100

    Delete binarytreechat directory

--- binarytreechat/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.binarytreechat {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.binarytreechat to javafx.fxml;
-    exports com.example.binarytreechat;
-}
