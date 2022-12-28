10949314925c249f148e9270c8be81a8bab307c2
Viktor-Spudil <viktor.spudil@stud.fh-campuswien.ac.at>
2022-12-28 19:12:04 +0100

    Beta classes of BasicMessage and Textmessage.

---
+++ binarytreechat/src/main/java/com/binarychat/testpackage/testclass.java
@@ -0,0 +1,15 @@
+package com.binarychat.testpackage;
+
+public class testclass {
+    // === 0. NOTES ===
+    // === 1. CLASS VARIABLES ===
+    // === 2. OBJECT VARIABLES ===
+    // === 3. CONSTRUCTORS ===
+    // --- 3.1 STATIC BLOCKS ---
+    // --- 3.2 INSTANCE INITIALIZER ---
+    // --- 3.3 REAL CONSTRUCTORS ---
+    // === 4. STATIC METHODS ===
+    // === 5. GETTER AND SETTER ===
+    // === 6. MISCELLANEOUS OBJECT METHODS ===
+    // === 7. MAIN ===
+}
