a41d061ee591bd665b1b6b2a177782cecc5e4e82
markrei <markus.reiterer@stud.fh-campuswien.ac.at>
2022-12-30 14:32:25 +0100

    BT-9

--- binarytreechat/src/main/java/com/binarychat/client/LoginScreenController.java
+++ binarytreechat/src/main/java/com/binarychat/client/LoginScreenController.java
@@ -2,8 +2,6 @@ package com.binarychat.client;
 
 import com.binarychat.IPAddress.checkIPAddress;
 import com.binarychat.IPAddress.checkPort;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.BooleanBinding;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
